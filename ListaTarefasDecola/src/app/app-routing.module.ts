5c98dcf8384431885d92b3585320ba0a0dea4186
Camila Monteiro <monteiro.camis@gmail.com>
2022-12-16 00:47:11 -0300

    :art: aula dia 14

--- ListaTarefasDecola/src/app/app-routing.module.ts
+++ ListaTarefasDecola/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { TarefasListaComponent } from './tarefas-lista/tarefas-lista.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TarefasDetalheComponent } from './tarefas-detalhe/tarefas-detalhe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'lista' , component: TarefasListaComponent},
+  {path: 'detalhe/:id' , component: TarefasDetalheComponent}, // /:atributo
+  {path: '**' , redirectTo:'lista'} // url desconhecida
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
